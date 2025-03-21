8e4ec848787b59fd2fa17e7f50ffc8b521310561
Phyo Thinzar Aung <phyothinzar.ag92@gmail.com>
2025-03-20 21:13:59 -0500

    Bug Fix: Event Item UI Test commit

--- app/src/androidTest/java/dev/phyo/tm_events/presentation/events/ui/EventItemKtTest.kt
+++ app/src/androidTest/java/dev/phyo/tm_events/presentation/events/ui/EventItemKtTest.kt
@@ -1,33 +1,19 @@
 package dev.phyo.tm_events.presentation.events.ui
 
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.onRoot
 import androidx.compose.ui.test.printToLog
-import androidx.compose.ui.unit.dp
-import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
-import com.bumptech.glide.integration.compose.GlideImage
 import dev.phyo.tm_events.domain.model.Event
 import org.junit.Rule
 import org.junit.Test
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.platform.testTag
-import androidx.compose.ui.test.onNodeWithTag
 
-@OptIn(ExperimentalGlideComposeApi::class)
 class EventItemKtTest{
 
     @get:Rule
     val composeTestRule = createComposeRule()
 
-
-    @Composable
     @Test
     fun testEventItem_displaysEventDetails(){
 
@@ -43,16 +29,6 @@ class EventItemKtTest{
             stateCode = "NY"
         )
 
-        GlideImage(
-            model = event.imageUrl,
-            contentDescription = event.name,
-            contentScale = ContentScale.Crop,
-            modifier = Modifier
-                .size(140.dp)
-                .clip(RoundedCornerShape(8.dp))
-                .testTag("eventImage")
-        )
-
         // WHEN
         composeTestRule.setContent {
             EventItem(event)
@@ -64,6 +40,5 @@ class EventItemKtTest{
         composeTestRule.onNodeWithText("2023-10-15").assertIsDisplayed()
         composeTestRule.onNodeWithText("Sample Venue").assertIsDisplayed()
         composeTestRule.onNodeWithText("New York, NY").assertIsDisplayed()
-        composeTestRule.onNodeWithTag("eventImage").assertExists()
     }
 }
